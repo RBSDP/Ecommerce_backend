9d0f18392f08c7af5f78d04d1d434023996e4e61
siva <raavibalasivadurgaprasad@gmail.com>
2023-07-26 15:53:49 +0530

    product schema added

--- model/coupon.schema.js
+++ model/coupon.schema.js
@@ -19,5 +19,10 @@ const couponSchema = new mongoose.Schema(
         timestamps:true
     }
 ) 
+// When you set timestamps:true in the schema definition, it means that the ORM or database system will automatically manage two special fields for each record in the table:
+
+// created_at: This field is automatically set to the timestamp when a new record is created.
+
+// updated_at: This field is automatically updated to the timestamp whenever a record is modified or updated.
 
 export default mongoose.model("Coupon",couponSchema)
--- model/product.schema.js
+++ model/product.schema.js
@@ -1,1 +1,58 @@
 import mongoose from "mongoose";
+
+
+const productSchema = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required : [true,"please provide a product name"],
+            trim:true,
+            maxLength : [120,"Product name should be a max of 120 characters"]
+
+            
+        },
+        price:{
+            type:Number,
+            required : [true,"please provide a product price"],
+            
+            maxLength : [5,"Product price should be a max of 5 digits"]
+            
+            
+        },
+        description:{
+            type:String,
+            //use some form of editor -assignment time - 26 min in part-3 class
+            
+            
+        },
+        photos:[
+            {
+                secure_url : {
+                    type:String ,
+                    required :true
+                }
+            }
+        ],
+        stock : {
+            type:Number ,
+            default : 0
+        },
+        sold:{
+            type:Number,
+            default : 0
+        },
+        // each product is part of collection , so store a reference of a collection
+        collectionId:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Collection'
+        }
+
+    },
+    {
+        timestamps:true 
+    }
+
+)
+
+
+export default mongoose.model('Product',productSchema)
