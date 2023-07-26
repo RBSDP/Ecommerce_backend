190c4732896e42e8231f0470f325aa6be6748d54
siva <raavibalasivadurgaprasad@gmail.com>
2023-07-26 15:33:51 +0530

    coupon schema added

--- model/user.schema.js
+++ model/user.schema.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
-
+import config from '../config/index'
 import AuthRoles from '../utils/authRoles'
+import JWT from 'jsonwebtoken'
+const bcrypt = require('bcryptjs'); //i did this way of import because it is giving error if i import using import keyword
+import crypto from 'crypto'
+
+
+
 
 const userSchema = mongoose.Schema(
     {
@@ -35,7 +41,49 @@ const userSchema = mongoose.Schema(
     }
 );
 
+// enum and timestanps learned
+
+//encrypt the password
+userSchema.pre('save',async function(next){
+    if(!this.modified('password')) return next();
+    this.password = await bcrypt.hash(this.password ,10)
+    next();
+})
+
+// add more features to the schema
+userSchema.methods = {
+    //compare password
+    comparePassword : async function(enteredPassword){
+        return await bcrypt.compare(enteredPassword,this.password)
+    },
+
+
+    // generate JWT token
+    getJwtToken: function(){
+        return JWT.sign({
+            _id:this._id,
+            role:this.role
+        },config.JWT_SECRET,{expiresIn:config.JWT_EXPIRY})
+    },
+
+    generateForgotPasswordToken: function(){
+        const forgotToken = crypto.randomBytes(20).toString('hex')
+        //step -1 -save to DB
+        this.forgotPasswordToken = crypto.createHash('sha256').update(forgotToken).digest('hex')
+
+
+        this.forgotPasswordExpiry = Date.now() + 20 * 60 * 1000
+        //step=2 =return valuse to user
+        return forgotToken
+    }
+
+
+}
+
 
 export default mongoose.model("User",userSchema);
+
+
+
 
 
