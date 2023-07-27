ba18615dcf16e6016fd4bb7836bd70057fdfffbd
siva <raavibalasivadurgaprasad@gmail.com>
2023-07-27 11:22:45 +0530

    mail helper is added

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -54,3 +54,70 @@ export const signUp = asyncHandler(async (req,res)=>{
         user 
     })
 })
+
+
+
+/* 
+
+@LOGIN
+@route http://localhost:4000/api/auth/login
+@description user signup controller for loging new user 
+@parameters email, password
+@return User Object
+
+*/
+
+export const login  = asyncHandler(async (req,res)=>{
+    const {email,password} = req.body 
+
+    if(!email || !password){
+        throw new CustomError('please fill all feilds',400)
+    }
+
+
+    const user = User.findOne({email}).select("+password")
+
+    if (!user){
+        throw new CustomError('Invalid credentials',400)
+    }
+
+
+    const isPasswordMatched = await user.comparePassword(password) 
+
+    if(isPasswordMatched){
+        const token  = user.getJwtToken()
+        user.password = undefined;
+        res.cookie('token',token,cookieOptions)
+        return res.status(200).json({
+            success:true,
+            token,
+            user
+        })
+    }
+
+    throw new CustomError('Invalid credentials -pass',400)
+})
+
+/* 
+
+@LOGOUT
+@route http://localhost:4000/api/auth/logout
+@description user logout by clearing user cookies
+@parameters 
+@return success message
+
+*/
+
+// below we use "_req" because we are not usinf req any where in the below code so as a good practise we are using.
+export const logout = asyncHandler(async(_req,res)=>{
+    res.cookie('token',null,{
+        expires:new Date(Date.now()),
+        httpOnly:true
+    })
+
+    res.status(200).json({
+        success:true,
+        message:'Logged Out'
+    })
+})
+
---
+++ config/transporter.config.js
@@ -0,0 +1,17 @@
+const nodemailer = require("nodemailer");
+import config from "./index";
+
+const transporter = nodemailer.createTransport({
+  host: config.SMTP_MAIL_HOST,
+  port: config.SMTP_MAIL_PORT,
+  secure: true,
+  auth: {
+    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+    user: config.SMTP_MAIL_USERNAME,
+    pass: config.SMTP_MAIL_PASSWORD
+  }
+});
+
+
+
+export default transporter
